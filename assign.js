36e8065ba42b9f0e9bb715d66021bc82cd0bf3f6
Mahalakshmi-14 <mahalakshmi.r@fibanolabs.com>
2022-05-30 19:34:07 +0530

    card created using json

---
+++ assign.js
@@ -0,0 +1,24 @@
+$(document).ready(function(e){
+    jsonObject.products.forEach((i)=>loadProducts(i));
+
+});
+
+// function example(){
+//     jsonObject.products.forEach((i)=>loadProducts(i));
+
+// }
+function loadProducts(data){
+
+var mydata = document.createElement('div');
+   mydata.innerHTML =  '<div class="box1">' +
+   '<img src="' + data.image +'">' +
+   '<div class="myname">' +
+   '<p class="p1">' + data.title +'</p>' +
+   '<p class="p2">' + data.description +'</p>' + 
+   '<p class="p3">' + data.content +'</p>' +
+   '</div>' +
+'</div>';
+document.getElementById("content1").appendChild(mydata);
+//console.log(data);
+}
+// example();
